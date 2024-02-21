5f3edbea1392b7d8f5726da1331c7b776b25dd70
ha9011 <gkehddnjs90@gmail.com>
2024-02-21 10:19:05 +0900

    jdbc 순수 등록수정삭제업데이트

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,13 +1,16 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repositoryV0 = new MemberRepositoryV0();
@@ -15,11 +18,25 @@ class MemberRepositoryV0Test {
     @Test
     @DisplayName("member save")
     void testName() throws SQLException {
-        // given
-        Member member = new Member("ahn", 1000);
-        // when
+
+        String memberid = "test3";
+        int money = 1000;
+        int updateMoney = 2000;
+        // save
+        Member member = new Member(memberid, money);
         repositoryV0.save(member);
-        // then
+
+        // findById
+        Member member1 = repositoryV0.findById(memberid);
+        log.info("member={}", member1);
+
+        assertThat(member).isEqualTo(member1);
+
+        repositoryV0.update(memberid, updateMoney);
+        Member updateMember = repositoryV0.findById(memberid);
+        assertThat(updateMember.getMoney()).isEqualTo(updateMoney);
+
+        repositoryV0.delete(memberid);
     }
 
 }
